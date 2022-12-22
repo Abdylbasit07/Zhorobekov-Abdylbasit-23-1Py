d5764c47459efd36b1b94b3b2d5a81976362e92f
Abdylbasit07 <zorobekovabdylbasit@gmail.com>
2022-12-22 14:53:24 +0600

    Update requirements.txt

--- products/views.py
+++ products/views.py
@@ -9,9 +9,9 @@ def main_view(request):
 def products_view(request):
     if request.method == 'GET':
         category_id = int(request.GET.get('category_id'))
-
         if category_id:
             products = Product.objects.filter(categories__in=[category_id])
+
         else:
             products = Product.objects.all()
 
